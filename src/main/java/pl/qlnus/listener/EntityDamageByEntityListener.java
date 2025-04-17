ed2e5604711daad9231b367befa4ac9ad3ef73a9
qlnus <qlnus1337@gmail.com>
2025-04-17 15:36:01 +0200

    Initial commit

---
+++ src/main/java/pl/qlnus/listener/EntityDamageByEntityListener.java
@@ -0,0 +1,10 @@
+package pl.qlnus.listener;
+
+/**
+ * Klasa: EntityDamageByEntityListener
+ * Autor: qlnus
+ * Data: 17.04.2025 15:29
+ * Projekt: DamageReduction-Plugin
+ */
+public class EntityDamageByEntityListener {
+}
